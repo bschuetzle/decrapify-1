d7e7df9d3c8a14272ff0b2dbad133950bb1eb7c8
drstrangele <chickenle@gmail.com>
2017-06-03 15:45:05 -0700

    made changes to doc

--- models/index.js
+++ models/index.js
@@ -1,2 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/decrapify");
+
+module.exports = {
+  Item: require('./items.js')
+}
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,7 +23,6 @@ $(document).ready(function() {
   */
 
   // Toggle tranparent navbar when the user scrolls the page
-
   $(window).scroll(function() {
     if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/
     {
