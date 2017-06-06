179ebf3021a412b391dc2d37d08821e310da13f7
drstrangele <chickenle@gmail.com>
2017-06-06 10:03:05 -0700

    made changes to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,12 +6,6 @@ $(document).ready(function() {
 
   renderDaysRemainingMsg("5934b14527172844a5eac072");
 
-  /*
-  **********************************************************
-  * OPAQUE NAVBAR SCRIPT
-  **********************************************************
-  */
-
   // Toggle tranparent navbar when the user scrolls the page
   $(window).scroll(function() {
     if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/
@@ -30,10 +24,12 @@ $(document).ready(function() {
   // $('.project').modal(options);
 
   // Items Page Click Event
-  // $('items-list').on('click', '.add-item', handleAddNewItem)
+  // $('.items-list').on('click', '.add-item', handleAddNewItem)
+  // $('.items-list').on('click', '.delete-item', handleDeleteItemClick);
 
 });
 
+// Render Items
 $.ajax({
   method: 'GET',
   url: '/api/items',
@@ -47,36 +43,48 @@ function renderMultipleItems(items) {
 }
 
 function renderItem(item) {
-  console.log('rendering item', item);
+
 
   var itemsHtml = (`
-<!-- items container! -->
-<div class="col-md-3">
-  <div class="panel panel-default">
-    <div class="panel-body">
-      <!-- image frame -->
-      <div class="row" data-items-id="${item._id}">
-        <div class="col-md-10 col-md-offset-1 items-header">
-          <h3>Item</h3>
-          <span class='item-description'>${item.description}<br>${item.category}</span>
-        </div>
-        <div class="col-md-10 col-md-offset-1 items-image">
-          <img src="${item.photoURL}" alt="item image">
-        </div>
-      </div>
-      <!-- yes or no box -->
-      <div class='panel-footer'>
-        <div class='panel-footer'>
-          <button class='btn btn-primary yes'>Yes!</button>
-          <button class='btn btn-danger no!'>No!</button>
+    <!-- items container! -->
+    <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-body">
+          <!-- image frame -->
+          <div class="row" data-items-id="${item._id}">
+            <div class="col-md-10 col-md-offset-1 items-header">
+              <span class='item-description'>${item.description}<br>${item.category}</span>
+            </div>
+            <div class="col-md-10 col-md-offset-1 items-image">
+              <img src="${item.photoURL}" alt="item image">
+            </div>
+          </div>
+          <!-- yes or no box -->
+          <div class='panel-footer'>
+            <div class='panel-footer'>
+              <button class='btn btn-primary yes'>Yes!</button>
+              <button class='btn btn-danger no!'>No!</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-`);
-$('#items-list').prepend(itemsHtml);
+    `);
+    $('#items-list').prepend(itemsHtml);
 }
+console.log($('#item-modal'));
+
+
+$('#item-modal').on('submit', function(e) {
+  e.preventDefault();
+  var formData = $(this).serialize();
+  console.log('formData', formData);
+  $.post('/api/items', formData, function(item) {
+    console.log('item after POST', item);
+    renderItem(item);  //render the server's response
+  });
+  $(this).trigger("reset");
+});
 
 
 
